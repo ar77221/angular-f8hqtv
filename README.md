5c18156dcee64b6b71df252de83e018e16b99330
ar77221 <ar_332@outlook.com>
2020-01-10 23:40:28 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-f8hqtv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-f8hqtv)
---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+
+  constructor(http: Http) { }
+
+  ngOnInit() {
+  }
+
+}
